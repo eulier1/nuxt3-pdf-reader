025349ff45f0935ff33fa22c91acde99e1e9ccba
Eulier <eulier@toolboxos.com>
2023-06-25 17:18:16 -0300

    scroll navigation

--- components/pagination.vue
+++ components/pagination.vue
@@ -1,35 +1,46 @@
 <template>
     <nav class="isolate inline-flex -space-x-px rounded-md shadow-sm" aria-label="Pagination">
-        <a href="#"
+        <!-- <a href="#"
             class="relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
             <span class="sr-only">Previous</span>
             <ChevronLeftIcon class="h-5 w-5" aria-hidden="true" />
-        </a>
+        </a> -->
         <!-- Current: "z-10 bg-indigo-600 text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600", Default: "text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:outline-offset-0" -->
-        <a href="#" aria-current="page"
-            class="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">1</a>
-        <a href="#"
-            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">2</a>
-        <a href="#"
-            class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">3</a>
-        <span
-            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-700 ring-1 ring-inset ring-gray-300 focus:outline-offset-0">...</span>
-        <a href="#"
-            class="relative hidden items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 md:inline-flex">8</a>
-        <a href="#"
-            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">9</a>
-        <a href="#"
-            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">10</a>
-        <a href="#"
+        <a v-for="page in pages" :key="page" href.prevent="#" @click="smoothScrolling(page)"
+            class="relative inline-flex items-center px-4 py-2 text-sm font-semibold text-gray-900 ring-1 ring-inset ring-gray-300 hover:bg-indigo-100 focus:z-20 focus:outline-offset-0"
+            :class="{ 'relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600': currentPage === page }
+                ">
+            {{ page }}
+        </a>
+        <!-- <a href="#"
             class="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0">
             <span class="sr-only">Next</span>
             <ChevronRightIcon class="h-5 w-5" aria-hidden="true" />
-        </a>
+        </a> -->
     </nav>
 </template>
 
 <script setup lang="ts">
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/vue/20/solid'
+
+const props = defineProps<{
+    pages: Number
+}>()
+
+const currentPage = ref(1)
+
+function smoothScrolling(pageNumber: number) {
+    const page: HTMLElement = document.getElementById(`vue-pdf-page-${pageNumber}`)
+    currentPage.value = pageNumber
+    console.log(page)
+    if (page) {
+        const offsetTop = page.offsetTop;
+        window.scrollTo({
+            top: offsetTop - 150,
+            behavior: 'smooth'
+        });
+    }
+}
 
 </script>
 
--- components/pdfRender.vue
+++ components/pdfRender.vue
@@ -1,11 +1,11 @@
 <template>
   <div>
-    <div class="flex justify-center p-8 bg-zinc-50 fixed w-screen top-8 z-10">
-      <pagination class=""></pagination>
+    <div class="flex justify-center p-8 bg-zinc-50 fixed w-screen top-8 z-10 shadow-xl">
+      <pagination :pages="numOfPages" class=""></pagination>
     </div>
 
     <div class="mt-40">
-      <VuePdf class="ml-8 mr-8 mb-8" v-for="page in numOfPages" :key="page" :src="pdfSrc" :page="page" />
+      <VuePdf class="ml-8 mr-8 mb-8" v-for="page in numOfPages" :key="page" :id="page" :src="pdfSrc" :page="page" />
     </div>
   </div>
 </template>
@@ -18,12 +18,29 @@ import { PDFDocumentProxy } from 'pdfjs-dist/types/src/display/api';
 const pdfSrc = ref<VuePdfPropsType['src']>('https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf')
 const numOfPages = ref(0)
 
-onMounted(() => {
-  const loadingTask = createLoadingTask(pdfSrc.value)
-  loadingTask.promise.then((pdf: PDFDocumentProxy) => {
-    numOfPages.value = pdf.numPages
-  })
+onMounted(async () => {
+  let docData = await getDocumentData(pdfSrc.value);
+  const pdf = await createLoadingTask(docData).promise
+  pdfSrc.value = docData
+  numOfPages.value = pdf.numPages
+  console.log(docData, pdf)
 })
+
+function getDocumentData(pdfPath: string): Promise<Uint8Array> {
+  return new Promise<Uint8Array>(function (resolve, reject) {
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', pdfPath);
+    xhr.responseType = 'arraybuffer';
+    xhr.onload = function () {
+      resolve(new Uint8Array(xhr.response));
+    };
+    xhr.onerror = function () {
+      reject(new Error('PDF is not loaded'));
+    };
+    xhr.send();
+  });
+}
+
 
 
 </script>
